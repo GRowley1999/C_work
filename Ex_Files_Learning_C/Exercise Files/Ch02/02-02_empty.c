b2cabd7bdda939a366a96388c838b8060f88822d
GRowley1999 <growley1999@hotmail.com>
2018-11-17 16:00:58 +0000

    Commit contents of exercise files from learning c course on lynda.com

---
+++ Ex_Files_Learning_C/Exercise Files/Ch02/02-02_empty.c
@@ -0,0 +1,12 @@
+/* Gregor Rowley 17/11/18 */
+#include <stdio.h>
+
+int main()
+{
+	// These puts statements print text to the output.
+	puts("Hey there!");
+	puts("Thrilled to meet you!");
+
+	return(0);
+}
+
